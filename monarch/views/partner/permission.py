7660ba1f6b6f1948308fb031ded4d60bc4afc78b
xiezhigang <zhigangxie@webot.co>
2020-03-10 18:55:22 +0800

    feat: 权限

--- monarch/views/partner/permission.py
+++ monarch/views/partner/permission.py
@@ -23,3 +23,24 @@ class Permission(Resource):
     def get(self):
         """获取权限列表"""
         return
+
+    @ns.doc("创建权限")
+    @ns.response(code=HTTPStatus.OK.value, description="创建权限")
+    @ns.response(code=HTTPStatus.BAD_REQUEST.value, description="参数错误")
+    def post(self):
+        """创建权限"""
+        return
+
+    @ns.doc("编辑权限")
+    @ns.response(code=HTTPStatus.OK.value, description="编辑权限")
+    @ns.response(code=HTTPStatus.BAD_REQUEST.value, description="参数错误")
+    def put(self):
+        """编辑权限"""
+        return
+
+    @ns.doc("删除权限")
+    @ns.response(code=HTTPStatus.OK.value, description="删除权限")
+    @ns.response(code=HTTPStatus.BAD_REQUEST.value, description="参数错误")
+    def delete(self):
+        """删除权限"""
+        return
