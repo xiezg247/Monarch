01e35853254cc66bd671dbcce17c5348ab991de0
xiezhigang <zhigangxie@webot.co>
2020-03-10 15:09:16 +0800

    feat: init local_settins

--- local_settings.py
+++ local_settings.py
@@ -1,4 +1,5 @@
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1:3306/monarch?charset=utf8mb4'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@182.92.164.95:18306/sso?charset=utf8mb4'
+
 SENTRY_DSN = ''
 REDIS_URL = 'redis://localhost:6379/0'
